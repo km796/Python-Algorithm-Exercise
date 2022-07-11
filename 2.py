8cf21e437ac29ce29a86e56a8b653a4553d979e1
km796 <31836577+km796@users.noreply.github.com>
2022-07-11 13:20:17 +0900

    2cond question of Bucket place

---
+++ 2.py
@@ -0,0 +1,15 @@
+def largestMagical(binString):
+
+    def helper(str):
+
+
+def count(s):
+    zeros = 0
+    ones = 0
+    for ch in s:
+        if ch == "0":
+            zeros += 1
+        else:
+            ones += 1
+
+    return [zeros, ones]
