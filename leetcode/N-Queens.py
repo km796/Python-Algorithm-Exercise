85aa7b771c4a63c0e79f2e208a22090736027b90
km796 <31836577+km796@users.noreply.github.com>
2022-07-21 14:24:26 +0900

    Nqueens backtrack approach basic

--- leetcode/N-Queens.py
+++ leetcode/N-Queens.py
@@ -8,9 +8,25 @@ class Solution:
 
         def backtrack(path, i, j, qs):
             if qs == n:
-                ans.add(path)
+                ans.add(tuple(map(tuple, path)))
                 return
-            self.check(path, i, j)
+            if i>=len(path) or j>=len(path[0]):
+                return
+            if self.check(path, i, j):
+                path[i][j] = 1
+                if j==len(path[0])-1:
+                    backtrack(path, i+1, 0, qs+1)
+                else:
+                    backtrack(path, i, j+1, qs+1)
+                path[i][j] = 0
+
+            if j==len(path[0])-1:
+                backtrack(path, i+1, 0, qs)
+            else:
+                backtrack(path, i, j+1, qs)
+        backtrack(board, 0, 0, 0)
+
+        return list(map(self.convert, ans))
 
     def check(self, path, i, j):
         for k in range(len(path)):
@@ -19,8 +35,18 @@ class Solution:
                 if 0 <= dir[0] < len(path) and 0 <= dir[1] < len(path) and path[dir[0]][dir[1]] == 1:
                     return False
         return True
+    def convert(self, path):
+        res = []
+        for row in path:
+            builder = ""
+            for r in row:
+                el = "." if r == 0 else "Q"
+                builder += el
+            res.append(builder)
+        return res
 
-
+sol = Solution()
+print(sol.solveNQueens(8))
 
 
 
